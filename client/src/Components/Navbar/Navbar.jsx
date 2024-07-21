f5c644d56b430d6946ea31a420713a225c15b263
Raunak1303 <161805979+Raunak1303@users.noreply.github.com>
2024-07-21 13:34:24 +0000

    updated navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import './Navbar.css'
-import iitp_logo from '../../assets/iitp-logo.png'
-
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import './Navbar.css';
+import iitp_logo from '../../assets/iitp-logo.png';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const [sticky, setSticky] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    useEffect(()=>{
-      window.addEventListener('scroll', ()=>{
-        window.scrollY > 50 ? setSticky(true) : setSticky(false);
-      })
-    },[])
+  const handleNavLinkClick = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-      <img src={iitp_logo} alt="logo" className='logo'/>
-      <ul className='navbar-links'>
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#about_us'>About us</a></li>
-        <li><a href='#clubs'>Clubs</a></li>
-        <li><a href='#events'>Events</a></li>
-        <li><a href='#teams'>Teams</a></li>
-        
-        <li><button className='btn'>Register</button></li>
+    <nav className="navbar dark-nav">
+      <img src={iitp_logo} alt="logo" className="logo" />
+      <div className={`menu-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+        <li><NavLink to="/" onClick={handleNavLinkClick}>Home</NavLink></li>
+        <li><a href="#about" onClick={handleNavLinkClick}>About Us</a></li>
+        <li><a href="#clubs" onClick={handleNavLinkClick}>Clubs</a></li>
+        <li><a href="#community" onClick={handleNavLinkClick}>Community</a></li>
+        <li><a href="#events" onClick={handleNavLinkClick}>Events</a></li>
+        <li><a href="#teams" onClick={handleNavLinkClick}>Teams</a></li>
+        <li><button className="btn" onClick={() => { navigate('/register'); setIsOpen(false); }}>Register</button></li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
